610e0b32cf3588a353556aff09f3c0d2e41e528e
Sanjay Sharma <sanjaygit256@gmail.com>
2026-07-05 21:55:17 +0530

    Upadtaed readme

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -6,7 +6,8 @@ public class Main {
         // Create a new board in starting position
         Bitboard board = new Bitboard();
         board.printBoard();
-        
+
+        // checking some function here (getPiece form Bitboard class)
         for(int i = 56; i < 64; i++) {
             System.out.println("Square " + i + ": " + board.getPiece(i));
         }
